3b5d7c382e77f2f9496f6d78c82f6803f0088e52
M4ndy <manasjidixit@gmail.com>
2021-12-04 16:52:43 +0530

    Print ASCII value of character through python

--- call tw.py
+++ call tw.py
@@ -1,11 +1,7 @@
-from twilio.rest import Client
-
-a = 'ACd4337ca12200600509f36bdf420eb583'
-at = 'f6b74f6b533ce7613a310d080069dd7e'
-client = Client(a,at)
-call = client.calls.create(
-twiml = '<Response><Say>Hello this is Vinay Mishra</Say></Response>',
-to='+917054908887',
-from_= '+13187089224'
-)
-print(call.sid)
+t = "oxx" * 1000
+s = input()
+if s in t:
+  print("Yes")
+else:
+  print("No")
+print(t)
